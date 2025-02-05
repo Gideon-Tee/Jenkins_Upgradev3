71867b56228b4f7457703efeb0d5b2df06b81fb1
Gideon Tettehfio <gideonlambride@gmail.com>
2025-02-05 09:22:44 +0000

    Added new test to pass

--- maven-samples/single-module/src/test/java/com/example/TestGreeter.java
+++ maven-samples/single-module/src/test/java/com/example/TestGreeter.java
@@ -27,7 +27,11 @@ public class TestGreeter {
     assertThat(greeter.greet(someone), containsString(someone));
   }
 
-  
+  @Test
+  public void NewTestToPass() {
+    assertTrue(true);
+
+  }
 
   @Test
   public void greetShouldIncludeGreetingPhrase() {
